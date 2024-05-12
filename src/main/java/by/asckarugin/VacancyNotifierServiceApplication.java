ade386e75b912e3b858f651f5b386b3134b904f9
Evgenii Krasikov <krasikov.ev2@gmail.com>
2024-05-12 14:55:40 +0500

    -init commit

---
+++ src/main/java/by/asckarugin/VacancyNotifierServiceApplication.java
@@ -0,0 +1,11 @@
+package by.asckarugin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacancyNotifierServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(VacancyNotifierServiceApplication.class, args);
+	}
+}
