a034f3d6fbb2234aeb3a2d0e33b707fc334605fb
Evgenii Krasikov <krasikov.ev2@gmail.com>
2024-05-17 19:36:12 +0500

    -add annotation

--- src/main/java/by/asckarugin/VacancyNotifierServiceApplication.java
+++ src/main/java/by/asckarugin/VacancyNotifierServiceApplication.java
@@ -2,8 +2,10 @@ package by.asckarugin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class VacancyNotifierServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(VacancyNotifierServiceApplication.class, args);
