1a6b2e56111b27c2a5f6e0189bc23bc9409f35cc
Evgenii Krasikov <krasikov.ev2@gmail.com>
2024-05-17 13:10:52 +0500

    -add value queue name

--- src/main/java/by/asckarugin/config/RabbitConfig.java
+++ src/main/java/by/asckarugin/config/RabbitConfig.java
@@ -6,12 +6,14 @@ import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitConfig {
 
+    @Value("${rabbitmq.consumer.new-vacancy-queue}")
     private String queueName;
 
     @Bean
