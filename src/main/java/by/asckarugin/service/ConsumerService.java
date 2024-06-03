77bf1b6e8d9335b1b6375f53b1889493cc5ea62c
Evgenii Krasikov <krasikov.ev2@gmail.com>
2024-06-03 20:12:10 +0500

    -form a text message about vacancy -for each subscribed user, send a message to telegram-notifications-queue -when a vacancy DTO is received, find users subscribed to its keyword

--- src/main/java/by/asckarugin/service/ConsumerService.java
+++ src/main/java/by/asckarugin/service/ConsumerService.java
@@ -1,13 +1,105 @@
 package by.asckarugin.service;
 
 import by.asckarugin.dto.hh.Vacancy;
+import by.asckarugin.dto.telegram.TelegramDto;
+import by.asckarugin.dto.telegram.UserDto;
+import by.asckarugin.repository.impl.UserRepositoryImpl;
+import org.jsoup.Jsoup;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ConsumerService {
+
+    private final UserRepositoryImpl userRepository;
+
+    private final ProducerService producerService;
+
+
+    public ConsumerService(UserRepositoryImpl userRepository, ProducerService producerService) {
+        this.userRepository = userRepository;
+        this.producerService = producerService;
+    }
+
     @RabbitListener(queues = "${rabbitmq.consumer.new-vacancies-queue}")
     public void consumeVacancy(Vacancy vacancy){
+        List<UserDto> users = userRepository.findUsersByQuery(vacancy.getQuery());
 
+        String message = formVacancyMessageText(vacancy);
+
+        sendMessageToTelegram(users, message);
     }
+
+    private void sendMessageToTelegram(List<UserDto> users, String message){
+        for(UserDto user: users){
+            if (user.getTelegramChatId() != null)
+                sendMessage(user.getTelegramChatId(), message);
+        }
+    }
+
+    private void sendMessage(Long chatId, String message){
+        TelegramDto telegramDto = TelegramDto.builder()
+                .chat_id(chatId)
+                .message(message)
+                .build();
+        producerService.sendMessage(telegramDto);
+    }
+
+    private String formVacancyMessageText(Vacancy vacancy){
+        StringBuilder stringBuilder = new StringBuilder("Новая вакансия для вас: ");
+        stringBuilder.append("<b>#")
+                .append(vacancy.getQuery())
+                .append("<b>#");
+        stringBuilder.append("\n");
+        stringBuilder.append(vacancy.getName());
+
+        if(vacancy.getEmployer() != null)
+            stringBuilder
+                    .append("\n")
+                    .append(vacancy.getArea().getName())
+                    .append("</b>");
+
+        if(vacancy.getArea() != null)
+            stringBuilder
+                    .append("\n")
+                    .append(vacancy.getArea().getName())
+                    .append("</b>");
+
+        if(vacancy.getSalary() != null){
+            String salaryFrom = String.valueOf(vacancy.getSalary().getFrom());
+            String salaryTo = String.valueOf(vacancy.getSalary().getTo());
+            if(!salaryTo.equals("0") || !salaryFrom.equals("0")){
+                stringBuilder.append("\n").append("Оплата: ");
+                if (!salaryFrom.equals("0"))
+                    stringBuilder.append("от ").append(salaryFrom).append(" ");
+
+                if(!salaryTo.equals("0"))
+                    stringBuilder.append("до ").append(salaryTo).append(" ");
+            }
+
+            stringBuilder.append(vacancy.getSalary().getCurrency());
+        }
+
+        if(vacancy.getSnippet() != null){
+            String requirement = vacancy.getSnippet().getRequirement();
+            String responsibility = vacancy.getSnippet().getResponsibility();
+            if(requirement != null)
+                stringBuilder.append("\n\n").append("Требования: ").append(deleteHtmlTags(requirement));
+
+            if(responsibility != null)
+                stringBuilder.append("\n\n").append("Обязанности: ").append(deleteHtmlTags(responsibility));
+        }
+
+        stringBuilder.append("\n");
+        stringBuilder.append("\n").append(vacancy.getAlternateUrl());
+
+        return stringBuilder.toString();
+    }
+
+    static String deleteHtmlTags(String html) {
+        return Jsoup.parse(html).text();
+    }
+
 }
