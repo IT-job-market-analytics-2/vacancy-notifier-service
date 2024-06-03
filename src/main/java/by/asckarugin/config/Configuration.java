eb6907f440fbb1d2653fd121b45b6efec22ef33a
Evgenii Krasikov <krasikov.ev2@gmail.com>
2024-06-03 19:09:48 +0500

    -add bean for restTemplate

---
+++ src/main/java/by/asckarugin/config/Configuration.java
@@ -0,0 +1,14 @@
+package by.asckarugin.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@org.springframework.context.annotation.Configuration
+public class Configuration {
+
+    @Bean
+    public RestTemplate template(){
+        return new RestTemplate();
+    }
+
+}
